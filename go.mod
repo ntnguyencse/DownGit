d9f8b0ccd6c537f465d01683df3f52f01cf024dc
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-06-25 09:22:31 +0000

    Init packages
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module downgit
+
+go 1.19
---
+++ src/downgit.go
@@ -0,0 +1,11 @@
+package downgit
+import (
+	_ "archive/zip"
+)
+func GetFile(path, url, file string) {
+
+}
+
+func ZipFile(path string, files []string) {
+
+}
